f819c201cba57ccdedb8a433c6aef2f265041313
Raymond Bello <raymondbello11@gmail.com>
2022-02-19 13:29:21 -0500

    Update doctest.h
    ios builds fail with 'unrecognized asm int $3'

--- doctest/doctest.h
+++ doctest/doctest.h
@@ -400,14 +400,16 @@ namespace doctest { namespace detail {
 #ifdef DOCTEST_PLATFORM_LINUX
 #if defined(__GNUC__) && (defined(__i386) || defined(__x86_64))
 // Break at the location of the failing check if possible
-#define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :) // NOLINT (hicpp-no-assembler)
+#define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :) // NOLINT (hicpp-no-assembler) ".inst 0xd4200000"
+// #define DOCTEST_BREAK_INTO_DEBUGGER() __asm__(".inst 0xd4200000") // NOLINT (hicpp-no-assembler) 
 #else
 #include <signal.h>
 #define DOCTEST_BREAK_INTO_DEBUGGER() raise(SIGTRAP)
 #endif
 #elif defined(DOCTEST_PLATFORM_MAC)
 #if defined(__x86_64) || defined(__x86_64__) || defined(__amd64__) || defined(__i386)
-#define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :) // NOLINT (hicpp-no-assembler)
+// #define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("int $3\n" : :) // NOLINT (hicpp-no-assembler)
+#define DOCTEST_BREAK_INTO_DEBUGGER() __asm__(".inst 0xd4200000") // NOLINT (hicpp-no-assembler) 
 #else
 #define DOCTEST_BREAK_INTO_DEBUGGER() __asm__("brk #0"); // NOLINT (hicpp-no-assembler)
 #endif
